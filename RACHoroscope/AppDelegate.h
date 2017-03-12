0eaba73281c2ee9a7112dfe4af6cea52691f3c6c
Victor Qi <qijianqiong@gmail.com>
2017-03-12 22:58:29 +0800

    Initial Commit

---
+++ RACHoroscope/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RACHoroscope
+//
+//  Created by v.q on 2017/3/12.
+//  Copyright © 2017年 Victor Qi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RACHoroscope/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RACHoroscope
+//
+//  Created by v.q on 2017/3/12.
+//  Copyright © 2017年 Victor Qi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
